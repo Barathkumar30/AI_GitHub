c1b60600bb47ec213e642b0c62416bdbf8977418
Barathkumar <m.barath012@gmail.com>
2025-02-25 16:20:15 +0530

    login page sucess

---
+++ src/app/(protected)/layout.tsx
@@ -0,0 +1,24 @@
+import { SidebarProvider } from '@/components/ui/sidebar'
+import { UserButton } from '@clerk/nextjs'
+import React from 'react'
+
+type props = {
+  children: React.ReactNode
+}
+
+const SidebarLayout = ({ children }: props) => {
+  return (
+    <SidebarProvider>
+      {/* <AppSidebar /> */}
+      <main className='w-full m-2'>
+        <div className='flex items-center gap-2 border-sidebar-border bg-sidebar border shadow rounded-md p-2 px-4'>
+          {/* <SearchBar /> */}
+          <div className="ml-auto"></div>
+          <UserButton />
+        </div>
+      </main>
+    </SidebarProvider>
+  )
+}
+
+export default SidebarLayout
